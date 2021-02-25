99aa0df6959db23a2552ecc8d8c9cb57e4c806fa
Filipe Dantas <filipedantas90@hotmail.com>
2021-02-25 19:02:55 -0300

    fourth day

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const {level} = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/flipdantas.png" alt="filipin"/>
@@ -8,7 +12,8 @@ export function Profile() {
         <strong>Filipe Dantas</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1</p>
+          Level {level}
+        </p>
       </div>
     </div>
   );
